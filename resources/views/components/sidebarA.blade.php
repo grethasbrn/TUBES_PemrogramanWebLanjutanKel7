f915df26020e857bad4ca4c355164c77588f4e57
Ratu Sabrina Turnip <rheyazaheira@gmail.comv>
2026-06-03 03:06:07 +0700

    logo pharmbe

--- resources/views/components/sidebarA.blade.php
+++ resources/views/components/sidebarA.blade.php
@@ -2,25 +2,36 @@
 <div class="topbar">
 
   <div class="logo">
-    <div class="logo-icon">💊</div>
+
+    <div class="logo-icon">
+      <img 
+        src="{{ asset('images/pharmbee-logo.png') }}" 
+        class="pharmbee-logo"
+        alt="Pharmbee"
+      >
+    </div>
 
     <div class="logo-text">
       <div class="l1">Pharmbee</div>
     </div>
+
   </div>
+
 
   <div class="topbar-right">
 
-    <!-- DATE & TIME -->
+    <!-- DATE -->
     <div class="date-chip">
       <div id="dateTag"></div>
     </div>
 
-    <!-- NOTIFICATION -->
+
+    <!-- NOTIF -->
     <a class="notif-btn" href="{{ url('apoteker/alerts') }}">
       <span>🔔</span>
       <span class="notif-badge" id="notifBadge"></span>
     </a>
+
 
     <!-- USER -->
     <div class="avatar-wrap">
@@ -56,7 +67,7 @@
   </a>
   <a class="nav-item {{ request()->is('apoteker/stock*') ? 'active' : '' }}" href="{{ url('apoteker/stock') }}">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M680-40v-120H560v-80h120v-120h80v120h120v80H760v120h-80ZM200-200v-560 560Zm0 80q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h560q33 0 56.5 23.5T840-760v353q-18-11-38-18t-42-11v-324H200v560h280q0 21 3 41t10 39H200Zm148.5-171.5Q360-303 360-320t-11.5-28.5Q337-360 320-360t-28.5 11.5Q280-337 280-320t11.5 28.5Q303-280 320-280t28.5-11.5Zm0-160Q360-463 360-480t-11.5-28.5Q337-520 320-520t-28.5 11.5Q280-497 280-480t11.5 28.5Q303-440 320-440t28.5-11.5Zm0-160Q360-623 360-640t-11.5-28.5Q337-680 320-680t-28.5 11.5Q280-657 280-640t11.5 28.5Q303-600 320-600t28.5-11.5ZM440-440h240v-80H440v80Zm0-160h240v-80H440v80Zm0 320h54q8-23 20-43t28-37H440v80Z"/></svg>
-    Stock
+    Stok
   </a>
   <a class="nav-item {{ request()->is('apoteker/alerts*') ? 'active' : '' }}" href="{{ url('apoteker/alerts') }}">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-80q-33 0-56.5-23.5T400-160h160q0 33-23.5 56.5T480-80Zm0-420ZM160-200v-80h80v-280q0-83 50-147.5T420-792v-28q0-25 17.5-42.5T480-880q25 0 42.5 17.5T540-820v13q-11 22-16 45t-4 47q-10-2-19.5-3.5T480-720q-66 0-113 47t-47 113v280h320v-257q18 8 38.5 12.5T720-520v240h80v80H160Z"/></svg>
@@ -65,7 +76,7 @@
   <div class="nav-section">Validation & Transactions</div>
   <a class="nav-item {{ request()->is('apoteker/prescription*') ? 'active' : '' }}" href="{{ url('apoteker/prescription') }}">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m678-134 46-46-64-64-46 46q-14 14-14 32t14 32q14 14 32 14t32-14Zm102-102 46-46q14-14 14-32t-14-32q-14-14-32-14t-32 14l-46 46 64 64ZM735-77q-37 37-89 37t-89-37q-37-37-37-89t37-89l148-148q37-37 89-37t89 37q37 37 37 89t-37 89L735-77ZM200-200v-560 560Zm0 80q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v245q-20-5-40-5t-40 3v-243H200v560h243q-3 20-3 40t5 40H200Z"/></svg>
-    Prescription
+    Resep
   </a>
   <a class="nav-item {{ request()->is('apoteker/invoice*') ? 'active' : '' }}" href="{{ url('apoteker/invoice') }}">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M240-80q-50 0-85-35t-35-85v-120h120v-560l60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60v680q0 50-35 85t-85 35H240Zm480-80q17 0 28.5-11.5T760-200v-560H320v440h360v120q0 17 11.5 28.5T720-160ZM360-600v-80h240v80H360Zm0 120v-80h240v80H360Zm320-120q-17 0-28.5-11.5T640-640q0-17 11.5-28.5T680-680q17 0 28.5 11.5T720-640q0 17-11.5 28.5T680-600Zm0 120q-17 0-28.5-11.5T640-520q0-17 11.5-28.5T680-560q17 0 28.5 11.5T720-520q0 17-11.5 28.5T680-480ZM240-160h360v-80H200v40q0 17 11.5 28.5T240-160Zm-40 0v-80 80Z"/></svg>
@@ -74,7 +85,7 @@
   <div class="nav-section">Reports</div>
   <a class="nav-item {{ request()->is('apoteker/report*') ? 'active' : '' }}" href="{{ url('apoteker/report') }}">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M120-120v-80l80-80v160h-80Zm160 0v-240l80-80v320h-80Zm160 0v-320l80 81v239h-80Zm160 0v-239l80-80v319h-80Zm160 0v-400l80-80v480h-80ZM120-327v-113l280-280 160 160 280-280v113L560-447 400-607 120-327Z"/></svg>
-    Report
+    Laporan
   </a>
 
   {{-- LOGOUT --}}
