ae5bc3764982cf4a2a9ed153112de60ce42a3f43
Ratu Sabrina Turnip <rheyazaheira@gmail.comv>
2026-06-05 23:16:39 +0700

    fixing eror

--- resources/views/components/sidebarAd.blade.php
+++ resources/views/components/sidebarAd.blade.php
@@ -20,71 +20,187 @@
 
   <div class="topbar-right">
 
-    <!-- DATE & TIME -->
+
     <div class="date-chip">
       <div id="dateTag"></div>
     </div>
 
-    <!-- USER -->
-    <div class="avatar-wrap">
 
-      <div class="avatar">
-        {{ strtoupper(substr(Auth::user()->name ?? 'A', 0, 1)) }}
-      </div>
+    <!-- USER DROPDOWN -->
+    <div class="profile-menu">
 
-      <div class="avatar-info">
+      <div class="avatar-wrap" onclick="toggleProfileMenu()">
 
-        <div class="avatar-name">
-          {{ Auth::user()->name ?? 'Admin RS' }}
+        <div class="avatar">
+          {{ strtoupper(substr(Auth::user()->name ?? 'A',0,1)) }}
         </div>
 
-        <div class="avatar-role">
-          {{ Auth::user()->role ?? 'Admin' }}
+
+        <div class="avatar-info">
+
+          <div class="avatar-name">
+            {{ Auth::user()->name ?? 'Admin RS' }}
+          </div>
+
+          <div class="avatar-role">
+            {{ Auth::user()->role ?? 'Admin' }}
+          </div>
+
         </div>
 
+
       </div>
+
+
+
+      <div class="profile-dropdown" id="profileDropdown">
+
+        <form method="POST" action="/logout">
+          @csrf
+
+          <button type="submit" class="logout-btn">
+            <i class="bi bi-box-arrow-right"></i>
+            Logout
+          </button>
+
+        </form>
+
+      </div>
+
 
     </div>
+
+
   </div>
 
+
 </div>
+
+
+
+
+
+
 <!-- SIDEBAR -->
 <div class="sidebar">
+
+
   <div class="nav-section">Menu</div>
-    <a class="nav-item {{ request()->is('admin/dashboard*') ? 'active' : '' }}" href="{{ url('admin/dashboard') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M183.5-183.5Q160-207 160-240t23.5-56.5Q207-320 240-320t56.5 23.5Q320-273 320-240t-23.5 56.5Q273-160 240-160t-56.5-23.5Zm240 0Q400-207 400-240t23.5-56.5Q447-320 480-320t56.5 23.5Q560-273 560-240t-23.5 56.5Q513-160 480-160t-56.5-23.5Zm240 0Q640-207 640-240t23.5-56.5Q687-320 720-320t56.5 23.5Q800-273 800-240t-23.5 56.5Q753-160 720-160t-56.5-23.5Zm-480-240Q160-447 160-480t23.5-56.5Q207-560 240-560t56.5 23.5Q320-513 320-480t-23.5 56.5Q273-400 240-400t-56.5-23.5Zm240 0Q400-447 400-480t23.5-56.5Q447-560 480-560t56.5 23.5Q560-513 560-480t-23.5 56.5Q513-400 480-400t-56.5-23.5Zm240 0Q640-447 640-480t23.5-56.5Q687-560 720-560t56.5 23.5Q800-513 800-480t-23.5 56.5Q753-400 720-400t-56.5-23.5Zm-480-240Q160-687 160-720t23.5-56.5Q207-800 240-800t56.5 23.5Q320-753 320-720t-23.5 56.5Q273-640 240-640t-56.5-23.5Zm240 0Q400-687 400-720t23.5-56.5Q447-800 480-800t56.5 23.5Q560-753 560-720t-23.5 56.5Q513-640 480-640t-56.5-23.5Zm240 0Q640-687 640-720t23.5-56.5Q687-800 720-800t56.5 23.5Q800-753 800-720t-23.5 56.5Q753-640 720-640t-56.5-23.5Z"/></svg>
-      Dashboard
-    </a>
-    <a class="nav-item {{ request()->is('admin/data*') ? 'active' : '' }}" href="{{ url('admin/data') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M555-435q-35-35-35-85t35-85q35-35 85-35t85 35q35 35 35 85t-35 85q-35 35-85 35t-85-35ZM400-160v-76q0-21 10-40t28-30q45-27 95.5-40.5T640-360q56 0 106.5 13.5T842-306q18 11 28 30t10 40v76H400Zm86-80h308q-35-20-74-30t-80-10q-41 0-80 10t-74 30Zm182.5-251.5Q680-503 680-520t-11.5-28.5Q657-560 640-560t-28.5 11.5Q600-537 600-520t11.5 28.5Q623-480 640-480t28.5-11.5ZM640-520Zm0 280ZM120-400v-80h320v80H120Zm0-320v-80h480v80H120Zm324 160H120v-80h360q-14 17-22.5 37T444-560Z"/></svg>
-      PatientData
-    </a>
-    <a class="nav-item {{ request()->is('admin/data*') && request()->has('validasi') ? 'active' : '' }}" href="{{ url('admin/data') }}#sec-validasi" onclick="setTimeout(()=>showSection('sec-validasi'),100)">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M380-300q-17 0-28.5-11.5T340-340v-160q0-17 11.5-28.5T380-540h160q17 0 28.5 11.5T580-500v160q0 17-11.5 28.5T540-300H380Zm20-60h120v-120H400v120Zm80-480q133 0 226.5 93.5T800-520q0 133-93.5 226.5T480-200q-133 0-226.5-93.5T160-520q0-133 93.5-226.5T480-840Zm0 80q-100 0-170 70t-70 170q0 100 70 170t170 70q100 0 170-70t70-170q0-100-70-170t-170-70Zm0 170Z"/></svg>
-      Validasi Pasien
-    </a>
-    <a class="nav-item {{ request()->is('admin/queue*') ? 'active' : '' }}" href="{{ url('admin/queue') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M538.5-138.5Q480-197 480-280t58.5-141.5Q597-480 680-480t141.5 58.5Q880-363 880-280t-58.5 141.5Q763-80 680-80t-141.5-58.5ZM747-185l28-28-75-75v-112h-40v128l87 87Zm-547 65q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h167q11-35 43-57.5t70-22.5q40 0 71.5 22.5T594-840h166q33 0 56.5 23.5T840-760v250q-18-13-38-22t-42-16v-212h-80v120H280v-120h-80v560h212q7 22 16 42t22 38H200Zm308.5-651.5Q520-783 520-800t-11.5-28.5Q497-840 480-840t-28.5 11.5Q440-817 440-800t11.5 28.5Q463-760 480-760t28.5-11.5Z"/></svg>
-      Queue
-    </a>
-    <a class="nav-item {{ request()->is('admin/dokter*') ? 'active' : '' }}" href="{{ url('admin/dokter') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/></svg>
-      Manajemen Dokter
-    </a>
 
-  <div class="nav-section">Patient</div>
-    <a class="nav-item {{ request()->is('admin/invoice*') ? 'active' : '' }}" href="{{ url('admin/invoice') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M240-80q-50 0-85-35t-35-85v-120h120v-560l60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60 60 60 60-60v680q0 50-35 85t-85 35H240Zm480-80q17 0 28.5-11.5T760-200v-560H320v440h360v120q0 17 11.5 28.5T720-160ZM360-600v-80h240v80H360Zm0 120v-80h240v80H360Zm320-120q-17 0-28.5-11.5T640-640q0-17 11.5-28.5T680-680q17 0 28.5 11.5T720-640q0 17-11.5 28.5T680-600Zm0 120q-17 0-28.5-11.5T640-520q0-17 11.5-28.5T680-560q17 0 28.5 11.5T720-520q0 17-11.5 28.5T680-480ZM240-160h360v-80H200v40q0 17 11.5 28.5T240-160Zm-40 0v-80 80Z"/></svg>
-      Invoice
-    </a>
-    <a class="nav-item {{ request()->is('admin/payment*') ? 'active' : '' }}" href="{{ url('admin/payment') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M560-440q-50 0-85-35t-35-85q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35ZM280-320q-33 0-56.5-23.5T200-400v-320q0-33 23.5-56.5T280-800h560q33 0 56.5 23.5T920-720v320q0 33-23.5 56.5T840-320H280Zm80-80h400q0-33 23.5-56.5T840-480v-160q-33 0-56.5-23.5T760-720H360q0 33-23.5 56.5T280-640v160q33 0 56.5 23.5T360-400Zm440 240H120q-33 0-56.5-23.5T40-240v-440h80v440h680v80ZM280-400v-320 320Z"/></svg>
-      Payment
-    </a>
 
-  <div class="nav-section">Report</div>
-    <a class="nav-item {{ request()->is('admin/report*') ? 'active' : '' }}" href="{{ url('admin/report') }}">
-      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M120-120v-80l80-80v160h-80Zm160 0v-240l80-80v320h-80Zm160 0v-320l80 81v239h-80Zm160 0v-239l80-80v319h-80Zm160 0v-400l80-80v480h-80ZM120-327v-113l280-280 160 160 280-280v113L560-447 400-607 120-327Z"/></svg>
-      Report
-    </a>
+
+  <a 
+    href="{{ url('admin/dashboard') }}"
+    class="nav-item {{ request()->is('admin/dashboard*') ? 'active' : '' }}"
+  >
+
+    <i class="bi bi-grid-1x2 nav-icon"></i>
+    Dashboard
+
+  </a>
+
+
+
+  <a 
+    href="{{ url('admin/data') }}"
+    class="nav-item {{ request()->is('admin/data*') ? 'active' : '' }}"
+  >
+
+    <i class="bi bi-people nav-icon"></i>
+    Data Pasien
+
+  </a>
+
+
+
+  <a 
+    href="{{ url('admin/invoice') }}"
+    class="nav-item {{ request()->is('admin/invoice*') ? 'active' : '' }}"
+  >
+
+    <i class="bi bi-receipt nav-icon"></i>
+    Invoice
+
+  </a>
+
+
+
+
+
+  <div class="nav-section">Laporan</div>
+
+
+
+  <a 
+    href="{{ url('admin/report') }}"
+    class="nav-item {{ request()->is('admin/report*') ? 'active' : '' }}"
+  >
+
+    <i class="bi bi-graph-up-arrow nav-icon"></i>
+    Laporan
+
+  </a>
+
+
+
 </div>
+
+
+
+
+
+
+
+
+
+<script>
+
+
+function updateDateTime(){
+
+
+    const now = new Date();
+
+
+    const tanggal = now.toLocaleDateString('id-ID',{
+        weekday:'long',
+        day:'numeric',
+        month:'long',
+        year:'numeric'
+    });
+
+
+    const jam = now.toLocaleTimeString('id-ID',{
+        hour:'2-digit',
+        minute:'2-digit',
+        second:'2-digit'
+    });
+
+
+
+    document.getElementById('dateTag').innerHTML =
+        `${tanggal} • ${jam}`;
+
+}
+
+
+
+
+function toggleProfileMenu(){
+
+    document
+      .getElementById('profileDropdown')
+      .classList
+      .toggle('show');
+
+}
+
+
+
+
+updateDateTime();
+
+setInterval(updateDateTime,1000);
+
+
+</script>
--- resources/views/components/sidebarD.blade.php
+++ resources/views/components/sidebarD.blade.php
@@ -17,27 +17,47 @@
 
   <div class="topbar-right">
 
-    <!-- DATE & TIME -->
+    <!-- DATE -->
     <div class="date-chip">
       <div id="dateTag"></div>
     </div>
 
-    <!-- USER -->
-    <div class="avatar-wrap">
 
-      <div class="avatar">
-        {{ strtoupper(substr(Auth::user()->name ?? 'D', 0, 1)) }}
+    <!-- USER DROPDOWN -->
+    <div class="profile-menu">
+
+      <div class="avatar-wrap" onclick="toggleProfileMenu()">
+
+        <div class="avatar">
+          {{ strtoupper(substr(Auth::user()->name ?? 'D', 0, 1)) }}
+        </div>
+
+        <div class="avatar-info">
+
+          <div class="avatar-name">
+            {{ Auth::user()->name ?? 'Dokter' }}
+          </div>
+
+          <div class="avatar-role">
+            {{ Auth::user()->role ?? 'Dokter' }}
+          </div>
+
+        </div>
+
       </div>
 
-      <div class="avatar-info">
 
-        <div class="avatar-name">
-          {{ Auth::user()->name ?? 'Dokter' }}
-        </div>
+      <div class="profile-dropdown" id="profileDropdown">
 
-        <div class="avatar-role">
-          {{ Auth::user()->role ?? 'Dokter' }}
-        </div>
+        <form method="POST" action="/logout">
+          @csrf
+
+          <button type="submit" class="logout-btn">
+            <i class="bi bi-box-arrow-right"></i>
+            Logout
+          </button>
+
+        </form>
 
       </div>
 
@@ -48,9 +68,13 @@
 </div>
 
 
+
+
+
 <div class="sidebar">
 
   <div class="nav-section">Menu</div>
+
 
   <div class="nav-item active" id="nav-dashboard" onclick="showSection('dashboard')">
     <i class="bi bi-grid-1x2 nav-icon"></i>
@@ -62,6 +86,7 @@
     <i class="bi bi-person-vcard nav-icon"></i>
     <span>Data Pasien</span>
   </div>
+
 
 
   <div class="nav-section">Resep</div>
@@ -79,6 +104,7 @@
   </div>
 
 
+
   <div class="nav-section">Laporan</div>
 
 
@@ -88,63 +114,50 @@
   </div>
 
 
-  <div class="nav-section">Akun</div>
-
-
-  <form method="POST" action="/logout">
-    @csrf
-
-    <button 
-      type="submit" 
-      class="nav-item" 
-      style="width:100%;background:none;border:none;cursor:pointer;text-align:left;padding-bottom:50px"
-    >
-
-      <svg 
-        xmlns="http://www.w3.org/2000/svg" 
-        height="24px" 
-        viewBox="0 -960 960 960" 
-        width="24px" 
-        fill="currentColor"
-      >
-        <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 
-        23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 
-        102-102H360v-80h327L585-622l55-58 
-        200 200-200 200Z"/>
-      </svg>
-
-      <span>Logout</span>
-
-    </button>
-
-  </form>
-
-
 </div>
+
+
+
 
 
 <script>
 
-function updateDateTime() {
+function updateDateTime(){
 
     const now = new Date();
 
-    const tanggal = now.toLocaleDateString('id-ID', {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric'
+
+    const tanggal = now.toLocaleDateString('id-ID',{
+        weekday:'long',
+        day:'numeric',
+        month:'long',
+        year:'numeric'
     });
 
-    const jam = now.toLocaleTimeString('id-ID', {
+
+    const jam = now.toLocaleTimeString('id-ID',{
         hour:'2-digit',
         minute:'2-digit',
         second:'2-digit'
     });
 
+
     document.getElementById('dateTag').innerHTML =
         `${tanggal} • ${jam}`;
+
 }
+
+
+
+function toggleProfileMenu(){
+
+    document
+      .getElementById('profileDropdown')
+      .classList
+      .toggle('show');
+
+}
+
 
 
 updateDateTime();
